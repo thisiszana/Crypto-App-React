74223dcc23792d4257dafc182e7ee5f728c525b5
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-02-13 12:36:37 +0330

    Implementing the TableCoin component and showing the desired items in the browser. and styling it.

--- src/Components/Modules/TableCoin.jsx
+++ src/Components/Modules/TableCoin.jsx
@@ -1,7 +1,66 @@
-function TableCoin() {
+import { RotatingLines } from "react-loader-spinner";
+import styles from "./TableCoin.module.css";
+
+import chartUp from "../../assets/chart-up.svg";
+import chartDown from "../../assets/chart-down.svg";
+
+function TableCoin({ coin, isLoading }) {
   return (
-    <div>TableCoin</div>
-  )
+    <div className={styles.container}>
+      {isLoading ? (
+        <RotatingLines strokeColor="#6f0477" strokeWidth="2" />
+      ) : (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>24h</th>
+              <th>Toatal Price</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {coin.map((coin) => (
+              <TableRow data={coin} key={coin.id} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
-export default TableCoin
+export default TableCoin;
+
+const TableRow = ({ data }) => {
+  const {
+    image,
+    name,
+    symbol,
+    current_price,
+    total_volume,
+    price_change_percentage_24h: change_price,
+  } = data;
+
+  return (
+    <tr>
+      <td>
+        <div className={styles.symbol}>
+          <img src={image} alt={name} />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>$ {current_price.toLocaleString()}</td>
+      <td className={change_price > 0 ? styles.success : styles.error}>
+        {change_price.toFixed(2)}%
+      </td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img src={change_price > 0 ? chartUp : chartDown} alt={name} />
+      </td>
+    </tr>
+  );
+};
--- src/Components/Template/HomePgae.jsx
+++ src/Components/Template/HomePgae.jsx
@@ -1,23 +1,30 @@
-import { useEffect, useState } from "react"
-import { getCoinList } from "../../Services/cryptoApi"
+import { useEffect, useState } from "react";
+import { getCoinList } from "../../Services/cryptoApi";
+import TableCoin from "../Modules/TableCoin";
 
 function HomePgae() {
-    const [coin, setCoin] = useState([]);
-    const [page, setPage] = useState(1);
-    const [currency, setCurrency] = useState("usd");
+  const [coin, setCoin] = useState([]);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("usd");
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const getData = async () => {
-            const res = await fetch(getCoinList(page, currency));
-            const json = await res.json();
-            setCoin(json);
-        }
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch(getCoinList(page, currency));
+      const json = await res.json();
+      
+      setCoin(json);
+      setIsLoading(false);
+    };
 
-        getData();
-    }, [])
+    getData();
+  }, []);
   return (
-    <div>HomePgae</div>
-  )
+    <>
+      <TableCoin coin={coin} isLoading={isLoading} />
+    </>
+  );
 }
 
-export default HomePgae
+export default HomePgae;
